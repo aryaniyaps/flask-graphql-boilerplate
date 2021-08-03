8b5fa91c4b825f9096ba7a2a916cb2e84f995702
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-08-03 08:32:52 +0530

    remove technical description from root fields

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/schema.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/schema.py
@@ -4,23 +4,12 @@ from {{ cookiecutter.project_slug }}.users.queries import UserQuery
 from {{ cookiecutter.project_slug }}.users.mutations import UserMutation
 
 
-class Query(UserQuery):
-    """
-    this is the root `query` field for the
-    GraphQL schema. This class must include
-    every query as it's method.
-    """
-    
+class Query(UserQuery):    
     # root node field definition.
     node = relay.Node.Field()
 
 
 class Mutation(UserMutation):
-    """
-    this is the root `mutation` field for 
-    the GraphQL schema. This class must include
-    every mutation as it's method.
-    """
     pass
 
 
