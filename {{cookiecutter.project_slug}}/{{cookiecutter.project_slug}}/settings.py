2ad10cdf00adb758623629a17e69c56ea678ce51
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-08-05 12:58:12 +0530

    update settings.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -11,9 +11,11 @@ PUBLIC_SITE_NAME = env.str("PUBLIC_SITE_NAME", default="{{ cookiecutter.project_
 ENV = env.str("FLASK_ENV", default="development")
 SECRET_KEY = env.str("SECRET_KEY")
 
-# flask-sqlalchemy configuration.
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# flask-mongoengine configuration.
+MONGODB_DB_SETTINGS = {
+    "host": env.str("DATABASE_URL"),
+    "connect": env.bool("MONGODB_CONNECT", default=True)
+}
 
 # flask-bcrypt configuration.
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=12)
