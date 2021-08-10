6ae73366aba38660bf94d17e3185d5cba5d71585
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-08-10 21:52:48 +0530

    update dataloaders

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/loaders.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/loaders.py
@@ -2,19 +2,17 @@ from aiodataloader import DataLoader
 
 from .models import User
 
-__all__ = (
-    "UserByIdLoader",
-    "UserByUsernameLoader"
-)
-
 
 class UserByIdLoader(DataLoader):
     async def batch_load_fn(self, keys):
-        users = User.objects.in_bulk(keys)
-        return [users.get(id=user_id) for user_id in keys]
+        user_map = User.objects.in_bulk(keys)
+        return [user_map.get(user_id) for user_id in keys]
 
 
 class UserByUsernameLoader(DataLoader):
     async def batch_load_fn(self, keys):
-        users = User.objects.filter(username__in=keys)
-        return [users.get(username=username) for username in keys]
+        users = User.objects(username__in=keys)
+        user_map = {}
+        for user in users:
+            user_map[user.username] = user
+        return [users.get(username) for username in keys]
